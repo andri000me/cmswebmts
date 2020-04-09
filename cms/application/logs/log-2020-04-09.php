ad97b9ad0ec786e35c9ac2de787a19d8ab89ebd4
faridpop <megatras@gmail.com>
2020-04-09 22:34:06 +0700

    firstup

---
+++ cms/application/logs/log-2020-04-09.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-04-09 21:55:40 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: No such host is known.  C:\xampp\htdocs\dndcms\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-04-09 21:55:40 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: No such host is known.  C:\xampp\htdocs\dndcms\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-04-09 21:55:40 --> Unable to connect to the database
+ERROR - 2020-04-09 22:01:14 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: No such host is known.  C:\xampp\htdocs\dndcms\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-04-09 22:01:14 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: No such host is known.  C:\xampp\htdocs\dndcms\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-04-09 22:01:14 --> Unable to connect to the database
+ERROR - 2020-04-09 22:07:37 --> Query error: Table 'webmts_db.tbl_settings' doesn't exist - Invalid query: SELECT * from tbl_settings WHERE id=1
+ERROR - 2020-04-09 22:11:17 --> Query error: Table 'webmts_db.tbl_settings' doesn't exist - Invalid query: SELECT * from tbl_settings WHERE id=1
